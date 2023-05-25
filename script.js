800d9530b49759d93a61a1b83470b9a4c5b620bb
blue butter <damiongallarza@gmail.com>
2023-05-24 20:21:56 -0700

    loop added in JS forecast api

--- script.js
+++ script.js
@@ -1,15 +1,54 @@
 // Access the form and weather info elements from the HTML
 const searchForm = document.getElementById('search-form');
 const weatherInfo = document.getElementById('weather-info');
+const futureWeatherDisplay = document.getElementById('future-weather');
 
-// Add event listener to the form submit event
-searchForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form submission
+// event listener for the form submit event
+searchForm.addEventListener('submit', function (event) {
+  event.preventDefault();
 
   const cityInput = document.getElementById('city-input').value;
-  const apiKey = '33ee9dfc864e17e04017196eff4eca3a'; 
+  const apiKey = '33ee9dfc864e17e04017196eff4eca3a';
 
   // Make API request to OpenWeatherMap
+  const fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}&units=metric`;
+
+  fetch(fiveDayUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      for (let i = 2; i < data.list.length; i += 8) {
+        const futureWeatherTemp = data.list[i].main.temp;
+        const futureWeatherDescription = data.list[i].weather[0].description
+        const futureWeatherDate = data.list[i].dt_txt;
+        console.log('---------------------')
+        console.log('temp: ', futureWeatherTemp);
+        console.log('description: ', futureWeatherDescription);
+        console.log('date: ', futureWeatherDate)
+        console.log('---------------------')
+
+        futureWeatherDisplay.innerHTML += `
+      <h1> ${futureWeatherDate}</h1>
+      <h2>Future Five Day Forecast in ${cityInput}</h2>
+      <p> Tempatures ${futureWeatherTemp}</p>
+      <p id="futureWeatherTemp"> Sky conditions ${futureWeatherDescription}</p>
+
+      `;
+
+
+
+
+
+
+
+      }
+    })
+
+    .catch(error => {
+      console.log('Error:', error);
+      futureWeatherDisplay.innerHTML = 'An error occurred while fetching weather data.';
+    });
+
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`)
     .then(response => response.json())
     .then(data => {
